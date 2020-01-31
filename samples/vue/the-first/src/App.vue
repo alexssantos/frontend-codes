4b95beb9fe7214769fb205f15dfa905b4abf9012
Alex Santos <arkanjo.alex@gmail.com>
2020-01-31 08:45:42 -0300

    Vue Paper Dashboard - Creative TIM Project Sample FREE

--- samples/vue/the-first/src/App.vue
+++ samples/vue/the-first/src/App.vue
@@ -3,6 +3,8 @@
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Alex's Vue Project."/>
     <Page2 msg="Essa é minha Pagina 2 em outro component."/>
+    <p v-if="Math.random() < 0.6">Está de dia / horas: {{horas}}</p>
+    <p v-if="Math.random() >= 0.6 ">Está de Tarde / horas: {{horas}}</p>
   </div>
 </template>
 
@@ -15,6 +17,12 @@ export default {
   components: {
     HelloWorld,
     Page2
+  },
+  data() {
+    return {
+      horas: new Data(),
+      hasHoras: true
+    }
   }
 }
 
--- samples/vue/the-first/src/components/Page2.vue
+++ samples/vue/the-first/src/components/Page2.vue
@@ -11,6 +11,8 @@ export default {
     msg: String
   }
 }
+
+
 </script>
 
 <style>

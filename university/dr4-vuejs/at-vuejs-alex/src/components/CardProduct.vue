8739a4c3daea5bdf8b94d849f6f5f2a5c2f66346
Alex Santos <arkanjo.alex@gmail.com>
2020-04-08 00:15:45 -0300

    Start AT VueJs
    - editar config tabs
    - add bootstrap 4.4
    - add fontawesome 4
    - home component
    - card produto component

---
+++ university/dr4-vuejs/at-vuejs-alex/src/components/CardProduct.vue
@@ -0,0 +1,130 @@
+<template>
+	<div class="wrapper">
+		<div class="container px-0">
+			<div class="top"></div>
+			<div class="bottom">
+				<div class="left">
+					<div class="details">
+						<h1 class="m-0 p-0">Chair</h1>
+						<p class="m-0 p-0">£250</p>
+					</div>
+					<div class="buy">
+						<i class="fas fa-cart-arrow-down"></i>						
+					</div>
+				</div>				
+			</div>
+		</div>
+		<div class="inside">
+			<div class="icon">
+				<i class="fas fa-info-circle"></i>
+			</div>			
+		</div> 
+	</div>
+</template>
+
+<script>
+export default {
+	name: "CardProduct",
+	data() {
+		return {};
+	}
+};
+</script>
+
+<style>
+.wrapper {
+	width: 300px;
+	height: 500px;
+	background: white;
+	margin: auto;
+	position: relative;
+	overflow: hidden;
+	border-radius: 10px 10px 10px 10px;
+	box-shadow: 0;
+	transform: scale(0.95);
+	transition: box-shadow 0.5s, transform 0.5s;
+}
+.wrapper:hover {
+	transform: scale(1);
+	box-shadow: 5px 20px 30px rgba(0, 0, 0, 0.2);
+}
+.wrapper .container {
+	width: 100%;
+	height: 100%;
+}
+
+/* TOP CARD */
+.wrapper .container .top {
+	height: 80%;
+	width: 100%;
+	background: url(https://s-media-cache-ak0.pinimg.com/736x/49/80/6f/49806f3f1c7483093855ebca1b8ae2c4.jpg)
+		no-repeat center center;	
+	background-size: 100%;
+}
+
+/* BOTTOM CARD */
+.wrapper .container .bottom {
+	width: 100%;
+	height: 20%;
+	transition: transform 0.5s;
+}
+.wrapper .container .bottom.clicked {
+	transform: translateX(-50%);
+}
+.wrapper .container .bottom .left {
+	height: 100%;
+	width: 100%;
+	background: #f4f4f4;
+	position: relative;
+	float: left;
+}
+.wrapper .container .bottom .left .details {
+	padding: 20px;
+	float: left;
+	width: calc(70% - 40px);
+}
+.wrapper .container .bottom .left .buy {
+	float: right;
+	width: calc(30% - 2px);
+	height: 100%;
+	background: #f1f1f1;
+	transition: background 0.5s;
+	border-left: solid thin rgba(0, 0, 0, 0.1);
+}
+.wrapper .container .bottom .left .buy i {
+	font-size: 30px;
+	padding: 30px;
+	color: #254053;
+	transition: transform 0.5s;
+}
+.wrapper .container .bottom .left .buy:hover {
+	background: #a6cdde;
+}
+.wrapper .container .bottom .left .buy:hover i {
+	transform: translateY(5px);
+	color: #00394b;
+}
+.wrapper .inside {
+	z-index: 9;
+	background: #a6cdde;
+	width: 140px;
+	height: 140px;
+	position: absolute;
+	top: -70px;
+	right: -70px;
+	border-radius: 0px 0px 200px 200px;
+	transition: all 0.5s, border-radius 2s, top 1s;
+	overflow: hidden;
+}
+.wrapper .inside .icon {
+	position: absolute;
+	right: 85px;
+	top: 85px;
+	color: white;
+	opacity: 1;
+}
+.wrapper .inside:hover .icon {
+	transform: scale(1.5);
+	transition:transform 0.3s ease-in-out;
+}
+</style>
---
+++ university/dr4-vuejs/at-vuejs-alex/src/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="hello">
+		<h1>PAGINA HOME</h1>	
+		<CardProduct/>
+	</div>
+</template>
+
+<script>
+
+import CardProduct from "./CardProduct";
+
+export default {
+	name: "Home",
+	components: {
+		CardProduct
+	},
+	props: {
+		msg: String
+	}
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>

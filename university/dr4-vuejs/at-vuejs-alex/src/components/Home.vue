1f82f027aa466eb27536ad58fa4c96e1fd7a2fa7
Alex Santos <arkanjo.alex@gmail.com>
2020-04-13 16:37:12 -0300

    Vuex instalado - lista de produtos no Store.state

--- university/dr4-vuejs/at-vuejs-alex/src/components/Home.vue
+++ university/dr4-vuejs/at-vuejs-alex/src/components/Home.vue
@@ -29,84 +29,14 @@ export default {
 		CardProduct
 	},
 	props: {},
+	computed: {
+		produtosLista() {
+			return this.$store.state.produtos
+		}
+	},
 	data() {
 		return {
-			"qtdCarrinho": 0,
-			"produtosLista": [
-				{
-					"id":0,
-					"nome": "Cake",
-					"preco": 279,
-					"categoria": "Doce",
-					"icon": "cake",
-				},{
-					"id":1,
-					"nome": "Apple",
-					"preco": 279,
-					"categoria": "Doce",
-					"icon": "apple",
-				},{
-					"id":2,
-					"nome": "Bread",
-					"preco": 279,
-					"categoria": "Doce",
-					"icon": "bread",
-				},{
-					"id":3,
-					"nome": "Carrot",
-					"preco": 279,
-					"categoria": "Doce",
-					"icon": "carrot",
-				},{
-					"id":4,
-					"nome": "Candy",
-					"preco": 279,
-					"categoria": "Doce",
-					"icon": "candy",
-				},{
-					"id":5,
-					"nome": "Coffee",
-					"preco": 279,
-					"categoria": "Doce",
-					"icon": "coffee",
-				},	{
-					"id":6,
-					"nome": "Cake",
-					"preco": 279,
-					"categoria": "Doce",
-					"icon": "cake",
-				},{
-					"id":7,
-					"nome": "Apple",
-					"preco": 279,
-					"categoria": "Doce",
-					"icon": "apple",
-				},{
-					"id":8,
-					"nome": "Bread",
-					"preco": 279,
-					"categoria": "Doce",
-					"icon": "bread",
-				},{
-					"id":9,
-					"nome": "Carrot",
-					"preco": 279,
-					"categoria": "Doce",
-					"icon": "carrot",
-				},{
-					"id":10,
-					"nome": "Candy",
-					"preco": 279,
-					"categoria": "Doce",
-					"icon": "candy",
-				},{
-					"id":11,
-					"nome": "Coffee",
-					"preco": 279,
-					"categoria": "Doce",
-					"icon": "coffee",
-				},
-			]
+			"qtdCarrinho": 0			
 		}
 	},
 	methods: {

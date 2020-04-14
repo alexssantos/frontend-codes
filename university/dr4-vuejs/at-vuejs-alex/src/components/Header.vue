1c6b181b37a35e579cb033985750d1e5c02f26f6
Alex Santos <arkanjo.alex@gmail.com>
2020-04-14 17:28:11 -0300

    Ajustes header

--- university/dr4-vuejs/at-vuejs-alex/src/components/Header.vue
+++ university/dr4-vuejs/at-vuejs-alex/src/components/Header.vue
@@ -4,14 +4,13 @@
 			<b-navbar-brand :to="{ name: 'home' }">
 				<i class="fas fa-store"></i>
 				Market Food
-			</b-navbar-brand>
-			<b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+			</b-navbar-brand>			
 
-			<b-navbar-nav>				
+			<b-navbar-nav>
 				<button
 					class="btn btn-outline-secondary mx-2 my-2 my-sm-0"
-					type="button"		
-					@click="goToHome()"			
+					type="button"
+					@click="goToHome()"
 				>
 					<i class="fas fa-home my-auto mx-2"></i>
 				</button>
@@ -19,37 +18,22 @@
 					class="btn btn-outline-secondary mx-2 my-2 my-sm-0"
 					type="button"
 					@click="createProduct()"
-				> 
-				<i class="fas fa-plus my-auto mx-2"></i>
-				Criar Produto
-				</button>				
+				>
+					<i class="fas fa-plus my-auto mx-2"></i>
+					Criar Produto
+				</button>
 			</b-navbar-nav>
 
-			<b-navbar-nav class="ml-auto">
-				<div class="row">
-					<b-nav-form>
-						<b-form-input
-							class="mr-sm-2"
-							placeholder="Search"
-						></b-form-input>
-						<button
-							size="sm"
-							class="my-2 my-sm-0 btn btn-outline-secondary"
-							type="button"
-						>
-							Search
-						</button>
-					</b-nav-form>
-					<button
-						class="btn btn-outline-secondary mx-3 my-2 my-sm-0 btn-cart"
-						type="button"
-					>
-						<i class="fas fa-shopping-cart my-auto mx-2">
-							12
-						</i>
-					</button>
-				</div>
-			</b-navbar-nav>
+			<div class="row ml-auto">
+				<button
+					class="btn btn-outline-secondary mx-3 my-2 my-sm-0 btn-cart"
+					type="button"
+				>
+					<i class="fas fa-shopping-cart my-auto mx-2">
+						12
+					</i>
+				</button>
+			</div>
 		</b-navbar>
 	</div>
 </template>
@@ -59,14 +43,17 @@
 export default {
 	name: "Header",
 	methods: {
-		goToHome(){
-			if (this.$route.name == 'home') return
-						
-			this.$router.push({name: 'home'})			
+		goToHome() {
+			if (this.checkIsHomePage()) return;
+
+			this.$router.push({ name: "home" });
 		},
-		createProduct(){
+		createProduct() {
 			// WARNING: obj $router dando ruim direto no template
-			this.$router.push({name:'create'})
+			this.$router.push({ name: "create" });
+		},
+		checkIsHomePage() {
+			return this.$route.name == "home";
 		}
 	}
 };
